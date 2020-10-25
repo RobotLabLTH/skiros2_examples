3d24785a1a29431d6cdfba6b77be085c4606507f
francesco <francesco@m-tech.aau.dk>
2020-10-25 09:02:48 -0700

    Improved simple params example

--- src/skiros2_examples/simple_params_example/simple_params_primitives.py
+++ src/skiros2_examples/simple_params_example/simple_params_primitives.py
@@ -1,6 +1,7 @@
-from skiros2_skill.core.skill import SkillDescription, ParamOptions
+import random
+import time
+from skiros2_skill.core.skill import SkillDescription
 from skiros2_common.core.params import ParamTypes
-from skiros2_common.core.world_element import Element
 from skiros2_common.core.primitive import PrimitiveBase
 import threading
 try:
@@ -12,40 +13,62 @@ except ImportError:
 # Descriptions
 #################################################################################
 
+
 class TrajectoryGenerator(SkillDescription):
     def createDescription(self):
-        #=======Params=========
+        # =======Params=========
         self.addParam("Trajectory", dict, ParamTypes.Optional)
-        self.addParam("Shutdown", False, ParamTypes.Optional)
+
 
 class TrajectoryConsumer(SkillDescription):
     def createDescription(self):
-        #=======Params=========
+        # =======Params=========
         self.addParam("Trajectory", dict, ParamTypes.Required)
-        self.addParam("Shutdown", bool, ParamTypes.Required)
 
 #################################################################################
 # Implementations
 #################################################################################
 
-import random
-import time
 
-class trajectory_generator(PrimitiveBase):
+class sync_trajectory_generator(PrimitiveBase):
     """
-    This primitive generates fake "trajectories", under form of dictionaries
-
-    To support a planning process that could take a long time, the plan function is executed in a parallel thread
-    and outputs a plan every second. The execute functions just updates the parameters.
-
-    Since parameters can be updated ONLY in the EXECUTE function, a syncronized queue is necessary
+    This primitive generates several fake trajectories, under form of a dictionary
     """
     q = queue.Queue(1)
-    is_done = False
-    iterations = 2
+    iterations = 5
 
     def onStart(self):
-        self.is_done = False
+        self.i = 0
+        return True
+
+    def createDescription(self):
+        self.setDescription(TrajectoryGenerator(), self.__class__.__name__)
+
+    def execute(self):
+        traj = {}
+        traj["Trajectory"] = [random.random() for _ in xrange(5)]
+        self.params["Trajectory"].values.append(traj)
+        self.i = self.i + 1
+        if self.i >= self.iterations:
+            return self.success("Planned trajectory: {}".format(self.params["Trajectory"].getValues()))
+        else:
+            return self.step("Planned trajectory: {}".format(self.params["Trajectory"].getValues()))
+
+
+class async_trajectory_generator(PrimitiveBase):
+    """
+    This primitive generates a fake "trajectory", under form of a dictionary
+
+    To support a planning process that could take a long time, the plan function
+    is executed in a parallel thread and outputs a plan after 2 seconds. The
+    execute functions just updates the parameters.
+
+    Since parameters can be updated ONLY in the EXECUTE function, a syncronized
+    queue is necessary
+    """
+
+    def onStart(self):
+        self.q = queue.Queue(1)
         self.worker = threading.Thread(target=self.plan)
         self.worker.start()
         return True
@@ -54,36 +77,29 @@ class trajectory_generator(PrimitiveBase):
         self.setDescription(TrajectoryGenerator(), self.__class__.__name__)
 
     def plan(self):
-        for i in range(0, self.iterations):
-            self.q.put([random.random() for _ in xrange(5)])
-            time.sleep(1)
-        self.is_done = True
+        time.sleep(2)
+        self.q.put([random.random() for _ in xrange(5)])
 
     def execute(self):
-        if self.is_done:
-            self.params["Shutdown"].setValue(True)
-            return self.success("Done")
         if not self.q.empty():
             traj = {}
             traj["Trajectory"] = self.q.get()
-            self.params["Trajectory"].addValue(traj)
-            return self.step("Added trajectory: {}".format(self.params["Trajectory"].getValues()))
-        return self.step("")
+            self.params["Trajectory"].value = traj
+            return self.success("Planned trajectory: {}".format(self.params["Trajectory"].getValues()))
+        return self.step("Planning...")
+
 
 class trajectory_consumer(PrimitiveBase):
     """
-    This primitive consumes one value in "trajectories" at each tick
-
-    It continues to run until the variable Shutdown is set to True
+    This primitive consumes one value in "Trajectory"
     """
+
     def createDescription(self):
         self.setDescription(TrajectoryConsumer(), self.__class__.__name__)
 
     def execute(self):
         if self.params["Trajectory"].isSpecified():
-            trajs = self.params["Trajectory"].getValues()
-            traj = trajs.pop(0)
-            return self.step("Consumed trajectory: {}".format(traj))
-        if self.params["Shutdown"].value:
-            return self.success("Done")
-        return self.step("")
+            trajs = self.params["Trajectory"].values
+            traj = trajs.pop()
+            return self.success("Consumed trajectory: {}".format(traj))
+        return self.success("Done")
--- src/skiros2_examples/simple_params_example/simple_params_skills.py
+++ src/skiros2_examples/simple_params_example/simple_params_skills.py
@@ -1,4 +1,4 @@
-from skiros2_skill.core.skill import SkillDescription, SkillBase, ParallelFf
+from skiros2_skill.core.skill import SkillDescription, SkillBase, ParallelFf, Serial
 from skiros2_common.core.params import ParamTypes
 from skiros2_common.core.world_element import Element
 
@@ -9,9 +9,7 @@ from skiros2_common.core.world_element import Element
 
 class TrajectoryCoordinator(SkillDescription):
     def createDescription(self):
-        #=======Params=========
-        #self.addParam("Container", Element(":Location"), ParamTypes.World)
-        #self.addParam("Object", Element(":Product"), ParamTypes.Optional)
+        # =======Params=========
         self.addParam("Pose", Element("skiros:TransformationPose"), ParamTypes.Optional)
         self.addParam("Pose2", Element("skiros:TransformationPose"), ParamTypes.Optional)
 
@@ -19,14 +17,25 @@ class TrajectoryCoordinator(SkillDescription):
 # Implementation
 #################################################################################
 
-class trajectory_coordinator(SkillBase):
+
+class async_trajectory_coordinator(SkillBase):
+    def createDescription(self):
+        self.setDescription(TrajectoryCoordinator(), self.__class__.__name__)
+
+    def expand(self, skill):
+        skill.setProcessor(Serial())
+        skill(
+            self.skill("TrajectoryGenerator", "async_trajectory_generator"),
+            self.skill("TrajectoryConsumer", "trajectory_consumer")
+        )
+
+class sync_trajectory_coordinator(SkillBase):
     def createDescription(self):
         self.setDescription(TrajectoryCoordinator(), self.__class__.__name__)
 
     def expand(self, skill):
         skill.setProcessor(ParallelFf())
         skill(
-            self.skill("TrajectoryGenerator", ""),
-            self.skill("TrajectoryConsumer", "")
+            self.skill("TrajectoryGenerator", "sync_trajectory_generator"),
+            self.skill("TrajectoryConsumer", "trajectory_consumer")
         )
-
